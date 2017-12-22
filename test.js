4fc7d104ae4d5f826358b14404d14c8de70e4dd1
Tyler Boright <tyler.boright@gmail.com>
2017-12-22 14:31:34 -0800

    changes the wordings of tests to be more clear

--- test.js
+++ test.js
@@ -7,19 +7,19 @@ test('should have methods on the module itself', t => {
     t.not(mod.random(), mod.random());
 });
 
-test('languages', t => {
+test('languages returns a set of all of the languages', t => {
     t.true(mod.languages.has('en'));
 });
 
-test('getQuote', t => {
+test('getQuote en by Id', t => {
     t.is(mod.getQuote(1, 'en'), "IT'S OVER 9000!!!");
 });
 
-test('getId', t => {
+test('getId en by Quote', t => {
     t.is(mod.getId("IT'S OVER 9000!!!", 'en'), 1);
 });
 
-test('all', t => {
+test('all should return all of the quotes for a lang', t => {
     t.not(mod.all().indexOf("IT'S OVER 9000!!!"), -1);
     t.not(mod.all().indexOf("Silly robot, do you really believe you have any chance against a Super Saiyan like me? Your circuits must be malfunctioning. Fresh out of the factory with no warranty and already broken. Such a pity."), -1);
 });
