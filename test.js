4df96c92affa2fd9b8ad37c0edeee26e0687a1d6
Tyler Boright <tyler.boright@gmail.com>
2017-12-22 14:21:45 -0800

    adds basic tests for the module and cleans up the imports

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-import test from 'ava
+import test from 'ava';
+import mod from '.';
 
-
+test('should have methods on the module itself', t => {
+    t.true(mod.all.length > 0);
+    t.truthy(mod.random());
+    t.not(mod.random(), mod.random());
+});
